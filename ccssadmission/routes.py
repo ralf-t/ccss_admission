4b5d7794a7857471b52418427e5e25c853462590
ralf-t <tabo.ralfmartin@ue.edu.ph>
2020-11-17 15:24:11 +0800

    final

--- ccssadmission/routes.py
+++ ccssadmission/routes.py
@@ -4,6 +4,7 @@ from ccssadmission import app, current_user
 @app.route("/")
 @app.route("/apply")
 def apply():
+	#edit
 	return render_template('apply.html')
 
 @app.route("/results")
@@ -13,33 +14,22 @@ def results():
 
 @app.route("/results/lrn")
 def results_lrn():
+	#edit
 	return render_template('results_lrn.html')
 
 @app.route("/login")
 def login():
-	#editorssss
-	#editorssss
-	#editorssss
-	#editorssss
-	#editorssss
-	#editorssss
-
+	#edit
 	return render_template('login.html')
 
 @app.route("/admin")
 def admin():
-	#editorssss
-	#editorssss
-	#editorssss
-	#editorssss
-	#editorssss
-	#editorssss
-
+	#edit
 	return render_template('admin.html')
 
 @app.route("/admin/applicant/id")
 def applicant():
-	#editorssss
+	#edit
 	return render_template('applicant.html')
 
 @app.route("/admin/applicant/id")
